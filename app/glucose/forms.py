27ef9f765af298d7dcc035adfd95f9f5246d9a46
Kacper Chruściel <kacperchrusciel777@gmail.com>
2025-06-03 16:54:19 +0200

    glucose

--- app/glucose/forms.py
+++ app/glucose/forms.py
@@ -3,7 +3,7 @@ from wtforms import IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 class GlucoseForm(FlaskForm):
-    glucose = IntegerField('Glucose Level (1-10)', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    glucose = IntegerField('Glucose Level (mg/dL)', validators=[DataRequired(), NumberRange(min=40, max=400)])
     note = TextAreaField('Note')
     submit = SubmitField('Save')
 
